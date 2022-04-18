f379ab988366817a085242c5f7c0c9c271bb78a7
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-04-18 16:03:40 +0600

    Pricing
    Pricing menu added and linked.

---
+++ pricing.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var a = document.getElementById("card1");
+    
+    if (x.innerHTML === "৳500") {
+      a.innerHTML = "৳5000";
+    } else {
+      x.innerHTML = "৳500";
+    }
+  }
+
+
+  
